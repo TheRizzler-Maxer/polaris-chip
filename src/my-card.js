ca1785719342e2e2e9243da022196b0054b7d7f8
Shane Godfrey <scg5440@psu.edu>
2025-02-18 13:18:36 -0500

    gdhdcf

--- src/my-card.js
+++ src/my-card.js
@@ -1,11 +1,5 @@
 import { LitElement, html, css } from 'lit';
 
-/**
- * Now it's your turn. Here's what we need to try and do:
- * 1. Get you HTML from your card working in here 
- * 2. Get your CSS rescoped as needed to work here
- */
-
 export class MyCard extends LitElement {
 
   static get tag() {
@@ -14,75 +8,109 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "Rick Deckard";
-    this.image = "https://www.therpf.com/forums/attachments/12417640_945743055507970_4938982837811334232_n-jpgoh16c74547ea87a653458bf8972775256aoe57d3cc87-jpg.759686/";
-    this.link = "https://bladerunner.fandom.com/wiki/Rick_Deckard";
-  }
-
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-      }
-      .card.toggled {
-        background-color: red;
-        color: blue;
-      }
-
-      .card {
-        background-color: #4f2a06;
-        width: 400px;
-        text-align: center;
-      }
-      .cardheader {
-        color: yellow;
-        margin: 10px 0px 10px 0px;
-        font-size: 40px;
-        font-family: "Times New Roman", Times, serif;
-      }
-      p {
-        color: yellow;
-        margin: 10px 25px 10px 25px;
-        font-family: "Times New Roman", Times, serif;
-      }
-      img {
-        width: 200px;
-        border: 4px solid yellow;
-      }
-      .btn {
-        color: #4f2a06;
-        background-color: white;
-        font-size: 15px;
-        margin: 0px 0px 5px 0px;
-      }
-      .btn:focus,
-      .btn:hover {
-        background-color: blue;
-        color: white;
-      }
-    `;
-  }
-
-  render() {
-    return html`
-    <div id = "cardlist">
-      <div class="card">
-        <h1 class="cardheader"><b>${this.title}</b></h1>
-          <img src=${this.image} alt=${this.title} />
-        <p>Deckard was a Blade Runner in the Fourth Sector until the slaughter at the steel shop. Now, he just wants to put the past behind him, maybe even start over in the off-world colonies. But in this town, the past has a habit of catching up with you.</p>
-        <a href=${this.link} target="_blank">
-          <button class="btn"><em>Details</em></button>
-        </a>
-      </div>
-    </div>`;
+    this.title = "Jared MCcain";
+    this.image = "https://static01.nyt.com/athletic/uploads/wp/2024/11/26003204/241126-Jared-McCain-scaled-e1732599303822.jpg";
+    this.link = "https://www.espn.com/nba/player/stats/_/id/4683778/jared-mccain";
+    this.fancy = false;
   }
 
   static get properties() {
     return {
       title: { type: String },
       image: { type: String },
-      link: { type: String},
+      link: { type: String },
+      fancy: { type: Boolean, reflect: true },
+      description: { type: String }
     };
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-block;
+        margin: 16px;
+      }
+      :host([fancy]) .card {
+        background-color: blue;
+        color: white;
+      }
+      .card.toggled {
+        background-color: red;
+        border: 2px solid black;
+        width: 400px;
+        color: blue;
+      }
+      .card {
+        background-color: blue;
+        width: 400px;
+        height: 600px
+        padding: 16px;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        text-align: center;
+        box-sizing; border-box;
+      }
+      .cardheader {
+        color: red;
+        margin: 10px 0;
+        font-size: 40px;
+        font-family: "Times New Roman", Times, serif;
+      }
+      img {
+        width: 100%; 
+        height: auto;
+        max-width: 600px;
+      }
+      .btn {
+        color: #4f2a06;
+        background-color: white;
+        font-size: 15px;
+        margin: 0 0 5px 0;
+      }
+      .btn:focus,
+      .btn:hover {
+        background-color: blue;
+        color: white;
+      }
+      details summary {
+        text-align: center;
+        font-size: 20px;
+        padding: 8px 0;
+        font-family: "Times New Roman", Times, serif;
+        color: yellow;
+      }
+      details[open] summary {
+        font-weight: bold;
+      }
+    `;
+  }
+
+  toggleFancy() {
+    this.fancy = !this.fancy;
+  }
+
+  openChanged(e) {
+    console.log(e.target.open);
+  }
+
+  render() {
+    return html`
+      <div class="card">
+        <h1 class="cardheader"><b>${this.title}</b></h1>
+        <img src=${this.image} alt=${this.title} />
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+          <summary>Description</summary>
+          <div>
+            <slot></slot>
+            <p>This is a Basketball Player</p>
+            <a href=${this.link} target="_blank">
+              <button class="btn"><em>Details</em></button>
+            </a>
+          </div>
+        </details>
+        <button class="btn" @click="${this.toggleFancy}">Toggle Fancy</button>
+      </div>
+    `;
   }
 }
 
